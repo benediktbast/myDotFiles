c57e69902847e3a9713529dcf09c4eade9db32dd
Benedikt Bast <mail@benediktbast.com>
2017-08-11 08:34:29 +0200

    fixing colorcodes and prompt init

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,5 +1,3 @@
-# https://github.com/blinks zsh theme
-
 #Auto completion
 autoload -Uz compinit
 compinit
@@ -8,7 +6,13 @@ setopt COMPLETE_ALIASES
 
 autoload -Uz promptinit
 promptinit
+prompt fade blue
 
+# 076 = Chartreuse3		#5fd700		rgb(95,215,0)	hsl(3,100%,42%)
+# 090	Red (SYSTEM)	#ff0000		rgb(255,0,0)	hsl(0,100%,50%)
+# 025	DeepSkyBlue4	#005faf		rgb(0,95,175)	hsl(07,100%,34%)
+# 075	SteelBlue1		#5fafff		rgb(95,175,255)	hsl(210,100%,68%)
+# 160	Red3			#d70000		rgb(215,0,0)	hsl(0,100%,42%)
 autoload colors && colors
 
 function _prompt_char() {
@@ -21,11 +25,11 @@ function _prompt_char() {
 
 ZSH_THEME_GIT_PROMPT_PREFIX=" [%{%B%F{076}%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{%f%k%b%B%F{white}%}]"
-ZSH_THEME_GIT_PROMPT_DIRTY=" %{%F{red}%}*%{%f%k%b%}"
+ZSH_THEME_GIT_PROMPT_DIRTY=" %{%F{160}%}*%{%f%k%b%}"
 ZSH_THEME_GIT_PROMPT_CLEAN=""
 
 PROMPT='%{%f%k%b%}
-%{%K{075}%B%F{white}%} %n%{%B%F{white}%}@%{%B%F{white}%}%m%{%B%F{white}%} %{%f%k%b%} %~%  %D{%f/%m/%y} %*%B%F{white}%}$(git_prompt_info)%E%{%f%k%b%}
+%{%K{025}%B%F{white}%} %n%{%B%F{white}%}@%{%B%F{white}%}%m%{%B%F{white}%} %{%f%k%b%} %~%  %D{%f/%m/%y} %*%B%F{white}%}$(git_prompt_info)%E%{%f%k%b%}
 %{%K{0}%}$(_prompt_char)$%{%f%k%b%} '
 
 RPROMPT='!%{%B%F{075}%}%!%{%f%k%b%}'

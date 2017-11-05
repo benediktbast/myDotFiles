ac8f1aa89b2f9fbb579fd4d49e60408968ac2ead
Benedikt Bast <mail@benediktbast.com>
2017-11-05 13:31:13 +0100

    adding short hash of commit to git prompt, only se system colors

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -8,26 +8,24 @@ autoload -Uz promptinit
 promptinit
 prompt fade blue
 
-# 076 = Chartreuse3		#5fd700		rgb(95,215,0)	hsl(3,100%,42%)
-# 090	Red (SYSTEM)	#ff0000		rgb(255,0,0)	hsl(0,100%,50%)
-# 025	DeepSkyBlue4	#005faf		rgb(0,95,175)	hsl(07,100%,34%)
-# 075	SteelBlue1		#5fafff		rgb(95,175,255)	hsl(210,100%,68%)
-# 160	Red3			#d70000		rgb(215,0,0)	hsl(0,100%,42%)
-# 15	White (SYSTEM)	#ffffff	rgb(255,255,255)	hsl(0,0%,100%)
+# 090   Red (SYSTEM)    #ff0000 rgb(255,0,0)        hsl(0,100%,50%)
+# 15    White (SYSTEM)  #ffffff rgb(255,255,255)    hsl(0,0%,100%)
 autoload colors && colors
 
 function _prompt_char() {
   if $(git rev-parse --is-inside-work-tree >/dev/null 2>&1); then
-    echo "%{%F{075}%}±%{%f%k%b%} "
+    echo "%{%F{blue}%}±%{%f%k%b%} "
   else
     echo ''
   fi
 }
 
-ZSH_THEME_GIT_PROMPT_PREFIX=" [%{%B%F{076}%}"
+ZSH_THEME_GIT_PROMPT_PREFIX=" [%{%B%F{green%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{%f%k%b%B%F{white}%}]"
-ZSH_THEME_GIT_PROMPT_DIRTY=" %{%F{160}%}*%{%f%k%b%}"
+ZSH_THEME_GIT_PROMPT_DIRTY=" %{%F{red}%}*%{%f%k%b%}"
 ZSH_THEME_GIT_PROMPT_CLEAN=""
+ZSH_THEME_GIT_PROMPT_SHA_BEFORE=" %{%f%k%b%B%F{white}%}"
+ZSH_THEME_GIT_PROMPT_SHA_AFTER="%{%f%k%b%}"
 
 PROMPT='%{%f%k%b%}
 %{%K{blue}%B%F{white}%}▓▒░%n%{%B%F{white}%}@%{%B%F{white}%}%m%{%B%F{white}%}░▒▓%{%f%k%b%} %~%  %D{%f/%m/%y} %*%B%F{white}%}$(git_prompt_info)%E%{%f%k%b%}

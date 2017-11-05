f9d9235f975b07cb68ae76e8b9e2a67e003c9c68
Benedikt Bast <mail@benediktbast.com>
2017-11-05 13:36:42 +0100

    fix wrong markup in GIT_PROMPT_PREFIX

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -20,7 +20,7 @@ function _prompt_char() {
   fi
 }
 
-ZSH_THEME_GIT_PROMPT_PREFIX=" [%{%B%F{green%}"
+ZSH_THEME_GIT_PROMPT_PREFIX=" [%{%B%F{green}%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{%f%k%b%B%F{white}%}]"
 ZSH_THEME_GIT_PROMPT_DIRTY=" %{%F{red}%}*%{%f%k%b%}"
 ZSH_THEME_GIT_PROMPT_CLEAN=""

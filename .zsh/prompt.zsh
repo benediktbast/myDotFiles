c032b57f0346b9393076d55ac53e0d14706706ff
Benedikt Bast <mail@benediktbast.com>
2017-11-05 13:34:37 +0100

    use default color for short sha

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -24,7 +24,7 @@ ZSH_THEME_GIT_PROMPT_PREFIX=" [%{%B%F{green%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{%f%k%b%B%F{white}%}]"
 ZSH_THEME_GIT_PROMPT_DIRTY=" %{%F{red}%}*%{%f%k%b%}"
 ZSH_THEME_GIT_PROMPT_CLEAN=""
-ZSH_THEME_GIT_PROMPT_SHA_BEFORE=" %{%f%k%b%B%F{white}%}"
+ZSH_THEME_GIT_PROMPT_SHA_BEFORE=" %{%f%k%b%}"
 ZSH_THEME_GIT_PROMPT_SHA_AFTER="%{%f%k%b%}"
 
 PROMPT='%{%f%k%b%}
